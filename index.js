f51e222a06a40ed8300cd1f93370f857c2d387ed
Oli Shah <oli_rahman2003@yahoo.co.uk>
2023-04-21 20:40:27 +0200

    Buy Button Added to Each Object

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ product.printName = function(){
 const products = [
     {name:"soap", price:2.99},
     {name:"sponge", price:0.99},
-    {name:"spatula", price:3},
+    {name:"spatula", price:3.00},
 ];
 
 const productContainer= document.getElementById("product-container");
@@ -16,8 +16,14 @@ const productContainer= document.getElementById("product-container");
 for (let i = 0; i < products.length; i++) {
     const product = products[i];
     const productElement = document.createElement("div");
+    const button = document.createElement("button");
+    button.textContent ="Buy";
     productElement.innerHTML = `<p>${product.name}: ${product.price}</p>`;
     productContainer.appendChild(productElement);
+    productContainer.appendChild(button);
+    
+
+    
   }
 
 
