e14d955d1edece72afbfe059feb26b72c84a7c5b
Oli Shah <oli_rahman2003@yahoo.co.uk>
2023-04-24 09:01:55 +0200

    Products Adds to One List and Prints to Console

--- index.js
+++ index.js
@@ -9,9 +9,11 @@ const products = [
     {name:"soap", price:2.99},
     {name:"sponge", price:0.99},
     {name:"spatula", price:3.00},
+    
 ];
 
 const productContainer= document.getElementById("product-container");
+let purchaseList = [];
 
 for (let i = 0; i < products.length; i++) {
     const product = products[i];
@@ -21,16 +23,38 @@ for (let i = 0; i < products.length; i++) {
     productElement.innerHTML = `<p>${product.name}: ${product.price}</p>`;
     productContainer.appendChild(productElement);
     productContainer.appendChild(button);
-
-    let purchaseList = [];
-
-  button.addEventListener("click",
+    
+    
+    
+    button.addEventListener("click",
     function(event) {
         purchaseList.push(product);
         console.log(purchaseList)
     }
 );
-  }
+}
+
+/*let purchaseListString = purchaseList.join(", ");
+let pressed = false;
+
+
+    myBasketBtn.addEventListener("click", 
+    function(event) {
+        if (!pressed) {
+            const productContainer = document.getElementById("product-container");
+
+            // createElement creates an element of the kind I indicate in the string.
+            const myBasketDisplay = document.createElement("a"); // creates anchor element
+            myBasketDisplay.textContent = purchaseListString; // sets the text inside
+            productContainer.appendChild(myBasketDisplay); // Finally, I add the new element as a child of an existing element.
+        }
+    }
+);*/
+
+
+
+
+
 
   
 
